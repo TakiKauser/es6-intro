6c631f80c722dd536feafdb34e6611077ceba821
TakiKauser <takikauserbadhon@gmail.com>
2021-08-25 01:46:55 +0600

    inheritance

--- inheritance.js
+++ inheritance.js
@@ -67,3 +67,18 @@ const taki = new Support("Taki", "Dhaka", 11);
 
 console.log(taki);
 // console.log(kauser);
+
+function min(nums) {  
+    return Math.min(nums) 
+}
+console.log(min( [1,3,2 ]));
+
+const cube=x=> x*x*x; 
+console.log(cube(2))
+
+const [a, b] = [1,2,3,4,45,5]; 
+console.log(a+b);
+
+const nums = [1,2,3,4,5]; 
+let output = nums.filter(n => n%2); 
+console.log(output);
