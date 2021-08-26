ac0c902e1206f7f7ed9e69515e813291a1f409c1
TakiKauser <takikauserbadhon@gmail.com>
2021-08-26 15:56:09 +0600

    done

--- es6-practice.js
+++ es6-practice.js
@@ -138,6 +138,26 @@ function containingThreeparameters(para1, para2, para3 = 7) {
 // ১১. একটা nested অবজেক্ট ডিক্লেয়ার করো (অর্থাৎ একটা অবজেক্ট এর প্রপার্টি এর মধ্যেও যে অবজেক্ট থাকতে পারে। আবার সেই অবজেক্ট এর প্রপার্টি এর মধ্যেও সে অবজেক্ট থাকতে পারে। সেই রকম একটা অবজেক্ট ডিক্লেয়ার করো। এবং যেকোন একটা প্রপার্টি এর মান একটা array হবে। জাস্ট এমন একটা অবজেক্ট )
 
 const nestedObject = {
+    office: {
+        name: "kb's software",
+        owner: {
+            name: "kb",
+            age: 29,
+            address: "Mymenshingh",
+            education: {
+                ssc: "GPA 5",
+                hsc: "GPA 5",
+                bsc: "CGPA 2.9"
+            },
+            expertice: ["git", "problem solving", "html5", "css3", "bootstrap", "tailwind", "javascript", "es6"]
+        },
+        address: "Dhaka",
+        employees: 27,
+        tech: ["html5", "css3", "bootstrap", "tailwind", "javascript", "es6"]
+    }
+};
+// console.log(nestedObject);
 
-}
-// ১২. উপরের অবজেক্ট এ ডট এর আগে যে প্রশ্নবোধক চিহ্ন দিয়ে যে অপশনাল চেইনিং করা যায়। সেটা একটু প্রাকটিস করো। 
+// ১২. উপরের অবজেক্ট এ ডট এর আগে যে প্রশ্নবোধক চিহ্ন দিয়ে যে অপশনাল চেইনিং করা যায়। সেটা একটু প্রাকটিস করো।
+
+console.log(nestedObject?.office?.owner?.expertice);
